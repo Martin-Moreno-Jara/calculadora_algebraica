9b0740310150f078d28fc6ecb3da350173d782d8
Martin Moreno <martin.moreno.j04@gmail.com>
2022-11-22 22:04:16 -0500

    prueba

--- algebra/Scripts/activate.bat
+++ algebra/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Downloads\algebra\algebra
+set VIRTUAL_ENV=C:\Users\Hexaw\Documents\calculadora algebraica\calculadora_algebraica\algebra
 
 if not defined PROMPT set PROMPT=$P$G
 
